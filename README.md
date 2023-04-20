11fb55be567152853509dbf49c5cb60529e64f2b
A. Bulangu <bulangu97@gmail.com>
2023-04-20 13:31:38 +0100

    readme

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # encrypt-decrypt
-## encrypt-decrypt, using those functions you can be able to encrypt and decrypt text and some multimedia files, with this function.
+## encrypt-decrypt, using AES-128-CBC Cyper can be able to encrypt and decrypt text and some multimedia files, with this function.
 
-#### Manual Use
-- [ https://www.php.net/manual/en/function.openssl-encrypt.php ] Openssl PHP.NET
-- [ https://www.php.net/manual/en/function.openssl-get-cipher-methods.php ] Cyper PHP.NET
+--
+    #### Manual Use
+    - [ https://www.php.net/manual/en/function.openssl-encrypt.php ] Openssl PHP.NET
+    - [ https://www.php.net/manual/en/function.openssl-get-cipher-methods.php ] Cyper PHP.NET
+
+
+    ##### How to use
+
+    --   $data =  array(
+            'final_name.mp3', 
+            '123456', 
+            'final_name.whatever'
+            
+        );
+        
+        AESencrypt($[0], $[1], $[2]);
--- encryptDecrypt.class.php
+++ encryptDecrypt.class.php
@@ -6,8 +6,72 @@
 * @ 
 */
 
-class Encrypt extends AnotherClass implements Interface
-{
+class ENCDECRYPT {
+    /**
+        * load 320kb into memory.
+    */
+
+    // AES ENCRYPT METHOD
+
+    public function AESencrypt($source, $key, $dest) {
+
+        $key = substr(sha1($key, true), 0, 16);
+        $iv = openssl_random_pseudo_bytes(16);
+
+        $err = false;
+        if ($fpOut = fopen($dest, 'w')) {
+
+            fwrite($fpOut, $iv);
+            if ($fpIn = fopen($source, 'rb')) {
+                while (!feof($fpIn)) {
+                    $plaintext = fread($fpIn, 16 * 20000);
+                    $ciphertext = openssl_encrypt($plaintext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                    $iv = substr($ciphertext, 0, 16);
+                    fwrite($fpOut, $ciphertext);
+                }
+                fclose($fpIn);
+            } else {
+                $err = true;
+            }
+            fclose($fpOut);
+        } else {
+            $err = true;
+        }
+
+        return $err ? false : $dest;
+
+    }
+
+    
+    public function AESdecrypt($source, $key, $dest){
+
+        $key = substr(sha1($key, true), 0, 16);
+
+        $err = false;
+        if ($fpOut = fopen($dest, 'w')) {
+            if ($fpIn = fopen($source, 'rb')) {
+                // Get the initialzation vector from the beginning of the file
+                $iv = fread($fpIn, 16);
+                while (!feof($fpIn)) {
+                    // we have to read one block more for decrypting than for encrypting
+                    $ciphertext = fread($fpIn, 16 * (20000 + 1)); 
+                    $plaintext = openssl_decrypt($ciphertext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                    // Use the first 16 bytes of the ciphertext as the next initialization vector
+                    $iv = substr($ciphertext, 0, 16);
+                    fwrite($fpOut, $plaintext);
+                }
+                fclose($fpIn);
+            } else {
+                $err = true;
+            }
+            fclose($fpOut);
+        } else {
+            $err = true;
+        }
+
+        return $err ? false : $dest;
+    }
+
     
 }
 
